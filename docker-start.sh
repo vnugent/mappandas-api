3c407a9f09061744de294faef2837b1e90dab957
Viet Nguyen <vietnguyen@noreply>
2018-12-30 12:01:38 -0800

    Capture stdout

--- docker-start.sh
+++ docker-start.sh
@@ -5,5 +5,15 @@ echo "Gunicorn workers: ${WORKERS}"
 if [ ! -z ${WSGI_DEBUG} ]; then
 	GUNICORN_LOG="--log-level DEBUG"
 fi
+echo "Checking required ENV variables"
+declare -a envlist=("DB_USER" "DB_PASS" "DB_HOST" "EMAIL_USER" "EMAIL_PASSWORD")
+for val in "${envlist[@]}"
+do
+  if [ -v ${!val} ]; then
+     echo "- $val=not defined!"
+  else
+     echo "- $val=****"
+  fi
+done
 
-exec gunicorn -w ${WORKERS} -b 0.0.0.0:8000 ${GUNICORN_LOG} --capture-output pandas:app
+#exec gunicorn -w ${WORKERS} -b 0.0.0.0:8000 ${GUNICORN_LOG} --enable-stdio-inheritance pandas:app
